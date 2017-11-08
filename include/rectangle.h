74bc3867ca707ef9b38dca7efb16f9e27a169602
Guillaume Secret <g.secret91@gmail.com>
2017-11-08 17:53:04 +0100

    DWAd

---
+++ include/rectangle.h
@@ -0,0 +1,36 @@
+#ifndef SFML_EXAMPLE_RECT_H
+#define SFML_EXAMPLE_RECT_H
+
+#include <utility.h>
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+enum class RectType
+{
+	PLAYER_CHARACTER,
+	GROUND,
+	NONE
+};
+
+class Rect
+{
+public:
+	Rect(sf::Vector2f position=sf::Vector2f(400,300),
+		sf::Vector2f size=sf::Vector2f(100,100));
+	~Rect();
+	void Move();
+	void init(b2World& world, b2BodyType bodyType);
+	void update(sf::Time dt);
+	void draw(sf::RenderWindow& window);
+	RectType GetRectType();
+	
+private:
+	sf::RectangleShape rectShape;
+	sf::Vector2f centerPos;
+	sf::Vector2f pixelSize;
+	b2Body* body = nullptr;
+	b2Fixture* box = nullptr;
+	RectType rectType;
+};
+
+#endif
---
+++ src/rectangle.cpp
@@ -0,0 +1,82 @@
+#include <rectangle.h>
+
+Rect::Rect(sf::Vector2f position, 
+	sf::Vector2f size)
+{
+	centerPos = position;
+	pixelSize = size;
+
+	
+}
+
+Rect::~Rect()
+{
+
+}
+
+void Rect::init(b2World& world, b2BodyType bodyType)
+{
+	switch (bodyType)
+	{
+	case b2_staticBody:
+		rectType = RectType::GROUND;
+		break;
+	case b2_dynamicBody:
+		rectType = RectType::PLAYER_CHARACTER;
+		break;
+	}
+	rectShape.setPosition(centerPos - pixelSize / 2.f);
+	rectShape.setSize(pixelSize);
+	rectShape.setFillColor(sf::Color::Red);
+
+	b2BodyDef bodyDef;
+	bodyDef.type = bodyType;
+	bodyDef.position = pixel2meter(centerPos);
+	bodyDef.fixedRotation = true;
+	bodyDef.userData = this;
+
+	body = world.CreateBody(&bodyDef);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.userData = this;
+	b2PolygonShape shape;
+	shape.SetAsBox(
+		pixel2meter(pixelSize.x / 2.f), pixel2meter(pixelSize.y / 2.f));
+	fixtureDef.shape = &shape;
+
+	box = body->CreateFixture(&fixtureDef);
+}
+
+// Moving and Jump
+void Rect::Move()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		body->SetLinearVelocity(b2Vec2(2.5f, body->GetLinearVelocity().y));
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		body->SetLinearVelocity(b2Vec2(-2.5f, body->GetLinearVelocity().y));
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) 
+	
+	{
+		body->SetLinearVelocity(b2Vec2(body->GetLinearVelocity().x, -5.5f));
+	}
+}
+
+void Rect::update(sf::Time dt)
+{
+	centerPos = meter2pixel(body->GetPosition());
+	rectShape.setPosition(centerPos - pixelSize / 4.f);
+}
+
+void Rect::draw(sf::RenderWindow& window)
+{
+	window.draw(rectShape);
+}
+
+RectType Rect::GetRectType()
+{
+	return rectType;
+}
